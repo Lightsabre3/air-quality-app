c3a307752d71322e347ba1464a9d47298abb8b8e
Siddharth Arun <siddhartharun306@gmail.com>
2024-11-09 11:46:09 -0800

    Updated to call the Air quality and Geo code APIs

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,20 @@ import React, { useState, useEffect } from 'react';
 
 const api = {
   key:'3f0401a085561142a56b12d0cc74f50f',
-  url: 'https://api.openweathermap.org/data/2.5/weather',
+  url: 'https://api.openweathermap.org/data/2.5/air_pollution',
 }
+
+const geo_api = {
+  key:'3f0401a085561142a56b12d0cc74f50f',
+  url: 'https://api.openweathermap.org/geo/1.0/direct',
+}
+
 
 function MyLocation() {
   const [position, setPosition] = useState({ latitude: null, longitude: null,heading: null });
 
   useEffect(() => {
+    console.log("Entered MyLocation")
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(function (position) {
         setPosition({
@@ -41,18 +48,37 @@ function MyLocation() {
 }
 
 const App = () => {
-  const [search, setSearch] = useState('London')
-  const [weather, setWeather] = useState({});
+  const [search, setSearch] = useState('Simi Valley')
+  const [airquality, setAirQuality] = useState({});
+  const [geo, setGeo] = useState({});
 
   const searchPressed = () => {
-    fetch(`${api.url}?q=${search}&units=metric&APPID=${api.key}`)
+    console.log("Search Pressed")
+    console.log(search)
+    
+    fetch(`${geo_api.url}?q=${search}&limit=5&APPID=${geo_api.key}`)
       .then((res) => res.json())
       .then((result) => {
-        setWeather(result)
-      })
+        console.log("after calling geo code API")
+        console.log(result)
+        setGeo(result)
+        })
       .catch((error) => {
         console.log(error)
       })
+
+      fetch(`${api.url}?lat=${geo[0].lat}&lon=${geo[0].lon}&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log("after calling air quality API")
+        console.log(result)
+        setAirQuality(result)
+        console.log(airquality.list[0].main.aqi)
+      })
+      .catch((error) => {
+        console.log(error)
+      })  
+
   }
 
 
@@ -67,11 +93,16 @@ const App = () => {
         <button onClick={searchPressed}>Search</button>
 
       <MyLocation />
-      {typeof weather.main != "undefined" ? <div>
-        <h2>City: { weather.name}</h2>
-        <p className='text-lg'>Weather :{ weather.main.temp}</p>
-        <p>Feels Like: {weather.main.feels_like}</p>
-        <p>Description: {weather.weather[0]["description"]}</p>
+      {typeof airquality.list[0].main != "undefined" ? <div>
+        <h2 className='text-lg'>Air Quality Index : {airquality.list[0].main.aqi}</h2>
+        <p>Concentration of Carbon Monoxide: {airquality.list[0].components.co} μg/m3</p>
+        <p>Concentration of Nitrogen Monoxide: {airquality.list[0].components.no} μg/m3</p>
+        <p>Concentration of Nitrogen Dioxide: {airquality.list[0].components.no2} μg/m3</p>
+        <p>Concentration of Ozone: {airquality.list[0].components.o3} μg/m3</p>
+        <p>Concentration of Sulphur Dioxide: {airquality.list[0].components.so2} μg/m3</p>
+        <p>Concentration of Fine Particles Matter: {airquality.list[0].components.pm2_5} μg/m3</p>
+        <p>Concentration of Coarse Particles Matter: {airquality.list[0].components.pm10} μg/m3</p>
+        <p>Concentration of Ammonia: {airquality.list[0].components.nh3} μg/m3</p>
       </div>
         : <p>Please correctly enter city name </p>}
 
